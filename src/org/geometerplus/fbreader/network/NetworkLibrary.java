b12cafd59617ce7feebb7a23516fd83609a85952
Dmitry Yuranov <d.yuranov@gmail.com>
2013-09-19 19:46:14 +0400

    "Catalogs manager" tree item

--- src/org/geometerplus/fbreader/network/NetworkLibrary.java
+++ src/org/geometerplus/fbreader/network/NetworkLibrary.java
@@ -492,6 +492,7 @@ public class NetworkLibrary {
 		// we do add non-catalog items
 		new SearchCatalogTree(myRootTree, mySearchItem, 0);
 		new AddCustomCatalogItemTree(myRootTree);
+		new AllCatalogsItemTree(myRootTree);
 
 		fireModelChangedEvent(ChangeListener.Code.SomeCode);
 	}
