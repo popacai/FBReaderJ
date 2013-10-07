8b142be2faf14c5ba9e8e9304cd1b1c1b8105e5b
wizard19900509 <wizard19900509@gmail.com>
2013-10-06 20:35:00 -0700

    Network

--- src/org/geometerplus/fbreader/sort/TitledEntity.java
+++ src/org/geometerplus/fbreader/sort/TitledEntity.java
@@ -90,20 +90,13 @@ public abstract class TitledEntity {
 	};
 	
 	static {
-		/*
 		ARTICLES.put("en", EN_ARTICLES);
 		ARTICLES.put("fr", FR_ARTICLES);
 		ARTICLES.put("de", GE_ARTICLES);
 		ARTICLES.put("it", IT_ARTICLES);
-		*/
 		ARTICLES.put("es", SP_ARTICLES);
 	}
 
-	public static void putArticles (String location, String[] articles)
-	{
-		ARTICLES.put(location, articles);
-		return ;
-	}
 	private static String trim(String s, String language) {
 		if (s == null) {
 			return "";
